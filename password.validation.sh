fc357359eb0479530d6ccd7a5d04b6676d9720e1
bryant-treacle <treacle.bryant@gmail.com>
2018-11-16 15:37:54 -0500

    Update password.validation.sh

--- password.validation.sh
+++ password.validation.sh
@@ -1,4 +1,22 @@
-#!/bin/bash
+#!/bin/bash -i
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Bryant Treacle
+# Date Created: 2018-11-16
+# Date last modified: 2018-11-16
+# Purpose: This script will replace the so-user-add script in Security Onion to enforce DoD complient password policies.
 
 #######################
 #  User Name function #
