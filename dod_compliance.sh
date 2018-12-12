731765c581ab309a9081ee0f761149df81f644b8
bryant-treacle <treacle.bryant@gmail.com>
2018-12-12 17:52:48 +0000

    Update dod_compliance.sh

--- dod_compliance.sh
+++ dod_compliance.sh
@@ -188,7 +188,7 @@ sudo systemctl restart ntp
 #######################################
 sysctl_conf()
 {
-sudo sed -i 's|#net.ipv4.tcp_syncookies = 1|net.ipv4.tcp_syncookies = 1|g' /etc/sysctl.conf
+sudo sed -i 's|#net.ipv4.tcp_syncookies=1|net.ipv4.tcp_syncookies = 1|g' /etc/sysctl.conf
 sudo sed -i 's|#net.ipv4.conf.default.accept_redirects = 0|net.ipv4.conf.default.accept_redirects = 0|g' /etc/sysctl.conf
 sudo sed -i 's|#net.ipv4.conf.all.send_redirects = 0|net.ipv4.conf.all.send_redirects = 0|g' /etc/sysctl.conf
 sudo sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward = 0|g' /etc/sysctl.conf
